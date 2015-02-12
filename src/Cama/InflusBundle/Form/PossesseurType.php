7d0d671cd69c87db5ddfdf95c3783dd2fce50d5e
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-12 13:35:32 +0100

    Update PossesseurType.php

--- src/Cama/InflusBundle/Form/PossesseurType.php
+++ src/Cama/InflusBundle/Form/PossesseurType.php
@@ -18,11 +18,11 @@ class PossesseurType extends AbstractType
             ->add('contacts', 'integer', array('max_length'=>1))
             ->add('ressources', 'integer', array('max_length'=>1))
 	    ->add('banque','integer')
-            ->add('servant', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des servants','type' => new ServantType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
-	    ->add('refuge', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des refuges','type' => new RefugeType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
+            ->add('servant', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des servants','type' => new ServantType(),'prototype'=>true,'by_reference' => false,'allow_add' => true,'allow_delete'=>true))
+	    ->add('refuge', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des refuges','type' => new RefugeType(),'prototype'=>true,'by_reference' => false,'allow_add' => true,'allow_delete'=>true))
 	    ->add('vehicule', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des véhicules','type' => new VehiculeType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
-            ->add('etiquette', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des compétences en étiquette','type' => new EtiquetteType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
-            ->add('influence', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des influences','type' => new InfluenceType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
+            ->add('etiquette', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des compétences en étiquette','type' => new EtiquetteType(),'prototype'=>true,'by_reference' => false,'allow_add' => true,'allow_delete'=>true))
+            ->add('influence', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des influences','type' => new InfluenceType(),'prototype'=>true,'by_reference' => false,'allow_add' => true,'allow_delete'=>true))
 	    ->add('save', 'submit',array("label"=>"Sauvegarder"));
     }
 
