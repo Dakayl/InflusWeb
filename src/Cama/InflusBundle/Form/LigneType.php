f752cce532bdf85b2fc2beac8480bb74fb0f03db
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-12 12:28:37 +0100

    Update LigneType.php

--- src/Cama/InflusBundle/Form/LigneType.php
+++ src/Cama/InflusBundle/Form/LigneType.php
@@ -2,6 +2,7 @@
 namespace Cama\InflusBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class LigneType extends AbstractType
 {
