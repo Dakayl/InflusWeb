ceed03b707b66f479ef7c6fb5feb874e9f36f0d5
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-12 11:53:41 +0100

    Update LignesTypes.php

--- src/Cama/InflusBundle/Form/LignesTypes.php
+++ src/Cama/InflusBundle/Form/LignesTypes.php
@@ -4,13 +4,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 class LignesType extends AbstractType
 {
-public function buildForm(FormBuilderInterface $builder, array $options)
-{
-$builder
-->add('value', 'integer');
-}
-public function getName()
-{
-  return 'lignes';
-}
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+    ->add('value', 'integer');
+  }
+  public function getName()
+  {
+    return 'lignes';
+  }
 }
