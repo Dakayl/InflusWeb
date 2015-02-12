042c2a6dd806919fbd7c411dbb846de95ae70ee3
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-12 14:18:56 +0100

    Update PnjController.php

--- src/Cama/InflusBundle/Controller/PnjController.php
+++ src/Cama/InflusBundle/Controller/PnjController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Cama\InflusBundle\Entity\Possesseur;
 use Cama\InflusBundle\Form\IAType;
 use Cama\InflusBundle\Constants;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class PnjController extends Controller
 {
