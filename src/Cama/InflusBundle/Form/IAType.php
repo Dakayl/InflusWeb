373dd88727ea7b6eb3e6ede8dfd0d5bc7225a91b
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-12 10:30:10 +0100

    Update IAType.php

--- src/Cama/InflusBundle/Form/IAType.php
+++ src/Cama/InflusBundle/Form/IAType.php
@@ -13,6 +13,7 @@ class IAType extends AbstractType
             ->add('joueur', 'text', array("label"=>"Gestionnaire"))
 	    ->add('clan','text', array('label'=>"Type"))
 	    ->add('ville','text')
+	    ->add('contacts', 'integer', array('max_length'=>1))
 	    ->add('etiquette', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des compétences en étiquette','type' => new EtiquetteType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
 	    ->add('influence', 'collection', array("label_attr"=>array("class"=>'headcoll'),"label"=>'Liste des influences','type' => new InfluenceType(),'prototype'=>true,'by_reference' => false,'allow_add' => true))
 	    ->add('save', 'submit',array("label"=>"Sauvegarder"));
