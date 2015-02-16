2329a503d5efd0f593fa859658cb78aa43456345
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-02-16 17:19:44 +0100

    Update Constants.php

--- src/Cama/InflusBundle/Constants.php
+++ src/Cama/InflusBundle/Constants.php
@@ -19,39 +19,105 @@ class Constants{
             'Ventrue'
         );
         
+        public static $list_disci  = array(
+	
+	"Disciplines Cardinales"=>array(
+		'AUS'=>'Auspex',
+	        'ANI'=>'Animalisme',
+	        'CEL'=>'Célérité',
+	        'DOM'=>'Domination',
+	        'DIS'=>	'Dissimulation',
+	        'FDA'=>	'Force d\'âme',
+	        'PRE'=>	'Présence',
+	        'PUI'=>	'Puissance'),
+       
+       "Disciplines Exotiques"=>array(
+        	'ALI'=>'Aliénation',
+        	'CHI'=>'Chimérie'
+        	'MET'=>'Métamorphose',
+        	'QUI'=>'Quietus',
+        	'SER'=>'Serpentis'),
+	
+	"Magies du Sang"=>array(
+	 	'THA'=>'Thaumaturgie', 
+	 	'NEC'=>'Nécromancie', 
+	 	'SOR'=>'Sorcellerie')
+	);
+	
+	public static $list_attr  = array(
+		"P"=>"Physique",
+		"S"=>"Social",
+		"M"=>"Mental"
+	);
+	
+	public static $list_mdt = array(
+	'Connaissances du MdT'=>array(
+		"VAM"=>"Vampires",
+		"SAB"=>"Sabbat",
+		"LUP"=>"Lupin",
+		"MAG"=>"Mages",
+		"FAE"=>"Changelins")
+	);
+	public static $list_comp = array(
+		'Talents'=>array(),
+		'Compétences'=>array(),
+		'Connaissances'=>array()
+	);
+
+        public static $list_learn_mentor = array_merge($list_comp, $list_disci, $list_mdt );
+        public static $list_learn_nomentor = array_merge($list_comp, $list_disci );
+        public static $list_influ = Constants::$TYPE_INFLUENCE;
+        $level = array(1,2,3,4,5);
+        public static $list_ville = array(
+        	 "LYO"=>"Lyon",
+        	 "ANI"=>"Amiens",
+        	 "ANG"=>"Angers",
+        	 "BOR"=>"Bordeaux"
+        	 "CHA"=>"Châteauroux",
+        	 "GRE"=>"Grenoble",
+        	 "LIL"=>"Lille",
+        	 "LIM"=>"Limoges",
+        	 "MEL"=>"Melun",
+        	 "MON"=>"Montpellier",
+        	 "ORL"=>"Orléans",
+        	 "PAR"=>"Paris",
+        	 "PER"=>"Périgueux",
+        	 "STR"=>"Strasbourg",
+        	 "YS"=>"Ys");
+        	 
         public static $LIST_ACTION = array(
             'Mentorat_D' => array(
             	'shorttext'=>'Donner un mentorat',
-            	'text'=>'Donner un mentorat de %1% à %2% (Joueur %3% Ville %4)',
-            	'parameters'=>array('list_learn','string','string','string')),
+            	'text'=>'Donner un mentorat de %1% %2% à %3% (Joueur %4% Ville %4)',
+            	'parameters'=>array('list_learn_mentor','level','string','string','list_ville')),
             'Mentorat_R' => array(
             	'shorttext'=>'Recevoir un mentorat',
-            	'text'=>'Recevoir un mentorat en %1% de %2% (Joueur %3% Ville %4)',
-            	'parameters'=>array('list_learn','string','string','string'));
-            'Apprendre_C' => array(
-            	'shorttext'=>'Apprendre une compétence seul',
-            	'text'=>'Apprendre la compétence %1% seul',
-            	'parameters'=>array('list_comp')),
-            'Apprendre_D' => array(
-            	'shorttext'=>'Apprendre une discipline seul',
-            	'text'=>'Apprendre la discipline %1% seul',
-            	'parameters'=>array('list_discipline')),
+            	'text'=>'Recevoir un mentorat en %1% %2% à %3% (Joueur %4% Ville %4)',
+            	'parameters'=>array('list_learn_mentor','level','string','string','list_ville'));
+            'Apprendre' => array(
+            	'shorttext'=>'Apprendre seul',
+            	'text'=>'Apprendre %1% %2% seul',
+            	'parameters'=>array('list_learn_nomentor','level')),
+    	    'Apprendre_A' => array(
+            	'shorttext'=>'Améliorer ses attribust',
+            	'text'=>'Apprendre l\'attribut %1% seul',
+            	'parameters'=>array('list_attr','level')),
             'Havre_T' => array(
             	'shorttext'=>'Trouver un havre',
             	'text'=>'Trouver un havre de niveau %1%',
-            	'parameters'=>array('integer')),
+            	'parameters'=>array('level')),
             'Havre_S' => array(
             	'shorttext'=>'Securiser votre havre',
-            	'text'=>'Sécuriser votre havre %1% avec sécurité niveau %2%',
-            	'parameters'=>array('list_refuge','integer')),
+            	'text'=>'Sécuriser votre havre %1% avec sécurité %2%',
+            	'parameters'=>array('list_refuge','level')),
             'Havre_S2' => array(
             	'shorttext'=>'Securiser un autre havre',
-            	'text'=>'Sécuriser le havre %1% avec sécurité niveau %2%',
-            	'parameters'=>array('string','integer')),
+            	'text'=>'Sécuriser le havre %1% avec sécurité %2%',
+            	'parameters'=>array('string','level')),
             'Serviteur' => array(
             	'shorttext'=>'Trouver un nouveau serviteur',
             	'text'=>'Trouver un nouveau serviteur de niveau %1%',
-            	'parameters'=>array('integer')),
+            	'parameters'=>array('level')),
             'Transforme' => array(
             	'shorttext'=>'Transformer votre serviteur en goule',
             	'text'=>'Transformer votre serviteur %1% en goule',
@@ -62,12 +128,12 @@ class Constants{
             	'parameters'=>array('string')),
             'Historique' => array(
             	'shorttext'=>'Augmenter son historique',
-            	'text'=>'Augmenter son historique %1%',
-            	'parameters'=>array('list_historique')),
+            	'text'=>'Augmenter son historique %1% niveau %2%',
+            	'parameters'=>array('list_historique','level')),
             'Developper' => array(
             	'shorttext'=>'Se développer sur un secteur',
             	'text'=>'Se développer sur le nouveau secteur d\'influence %1%','
-            	parameters'=>array('list_secteurs')),
+            	parameters'=>array('list_influ')),
             'Espionner' => array(
             	'shorttext'=>'Obtenir des renseignements sur un personnage',
             	'text'=>'Obtenir des renseignements sur %1%',
@@ -79,15 +145,15 @@ class Constants{
         );
         
 	public static $LIST_ORDRES = array(
-		'A'=>array(text=>'Attaque',target=>true),
-		'D'=>array(text=>'Défense',target=>false),
-		'I'=>array(text=>'Dissimuler',target=>false),
-		'P'=>array(text=>'Pister',target=>true),
-		'E'=>array(text=>'Espionner',target=>true),
-		'O'=>array(text=>'Observer',target=>false),
-		'S'=>array(text=>'Spécifique',target=>false),
-		'C'=>array(text=>'Croissance',target=>false),
-		'T'=>array(text=>'Transfert',target=>true)
+		'A'=>array('text'=>'Attaque','target'=>true),
+		'D'=>array('text'=>'Défense','target'=>false),
+		'I'=>array('text'=>'Dissimuler','target'=>false),
+		'P'=>array('text'=>'Pister','target'=>true),
+		'E'=>array('text'=>'Espionner','target'=>true),
+		'O'=>array('text'=>'Observer','target'=>false),
+		'S'=>array('text'=>'Spécifique','target'=>false),
+		'C'=>array('text'=>'Croissance','target'=>false),
+		'T'=>array('text'=>'Transfert','target'=>true)
 	);
 
 	public static $TYPE_SERVANT = array(	
