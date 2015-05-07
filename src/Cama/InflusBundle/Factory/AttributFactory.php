6bb204704a7f42af086f6a80c2105714c9c93f18
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-05-07 14:09:25 +0200

    Update AttributFactory.php

--- src/Cama/InflusBundle/Factory/AttributFactory.php
+++ src/Cama/InflusBundle/Factory/AttributFactory.php
@@ -1,5 +1,32 @@
 <?php
+use Cama\InflusBundle\Entity\Attribut;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class AttributFactory {
-
+  protected $player = null;
+  
+  public function __construct(){
+    
+  }
+  
+  public function setPlayer($pid) {
+    $this->player = $pid;
+  }
+  
+  public function getPlayer() {
+    return $this->player;
+  }
+  
+  
+  public function deleteAll(){
+    if(empty($this->player)) return 0;
+    // To do
+  }
+  
+  public function createFromRequest(Request $request){
+    if(empty($this->player)) return 0;
+    // To do
+  }
+  
 }
