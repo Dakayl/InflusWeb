270a17af2ff25d18d14053504be9c0b421c51bb6
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-05-07 14:10:26 +0200

    Update OrdreFactory.php

--- src/Cama/InflusBundle/Factory/OrdreFactory.php
+++ src/Cama/InflusBundle/Factory/OrdreFactory.php
@@ -1,5 +1,31 @@
 <?php
+use Cama\InflusBundle\Entity\Ordre;
+use Symfony\Component\HttpFoundation\Request;
 
-classe OrdreFactory {
-
+class OrdreFactory {
+  
+  protected $player = null;
+  public function __construct(){
+    
+  }
+  
+  public function setPlayer($pid) {
+    $this->player = $pid;
+  }
+  
+  public function getPlayer() {
+    return $this->player;
+  }
+  
+  
+  public function deleteAll(){
+    if(empty($this->player)) return 0;
+    // To do
+  }
+  
+  public function createFromRequest(Request $request){
+    if(empty($this->player)) return 0;
+    // To do
+  }
+  
 }
